9e8adf4a3a2eb02c442530bcbc7246f7d1dd9540
Zovma <dim.borzov@gmail.com>
2022-02-26 00:37:16 +0300

    POST ajax, write json file

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const fs = require("fs");  
 const app = express();
- 
-app.use(function (request, response) {
-  response.sendFile(__dirname + "/formula.html");
+// создаем парсер для данных в формате json
+const jsonParser = express.json();
+  
+app.post("/result", jsonParser, function (request, response) {
+    console.log(request.body);
+    // fs.writeFileSync("result.json", request);
+    if(!request.body) return response.sendStatus(400);
+    response.json(request.body); // отправляем пришедший ответ обратно
+    // let arr = JSON.parse(request.body);
+    // fs.writeFileSync("result.json", request);
+    // let obj = request.body
+    fs.writeFileSync("result.json", JSON.stringify(request.body));
 });
- 
+  
+app.get("/", function(request, response){
+      
+    response.sendFile(__dirname + "/formula.html");
+});
+  
 app.listen(3000);
