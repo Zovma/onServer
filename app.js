760651baf6938d178c3e3d1c771280d02aef817b
Zovma <dim.borzov@gmail.com>
2022-02-26 15:54:29 +0300

    fixed the bug

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const express = require("express");
-const fs = require("fs");  
+const fs = require("fs");
 const app = express();
 // создаем парсер для данных в формате json
 const jsonParser = express.json();
-  
+
 app.post("/result", jsonParser, function (request, response) {
+    // console.log(request.body);
     // fs.writeFileSync("result.json", request);
-    if(!request.body) return response.sendStatus(400);
+    if (!request.body) return response.sendStatus(400);
     response.json(request.body); // отправляем пришедший ответ обратно
     // let arr = JSON.parse(request.body);
     // fs.writeFileSync("result.json", request);
     // let obj = request.body
     fs.writeFileSync("result.json", JSON.stringify(request.body));
 });
-  
-app.get("/", function(request, response){
-      
+
+app.get("/", function (request, response) {
+
     response.sendFile(__dirname + "/formula.html");
 });
 
-app.get("/json", function(request, response){
-      
-    response.sendFile(__dirname + "/result.json");
+app.get("/json", function (request, response) {
+
+        response.sendFile(__dirname + "/result.json")
+
 });
-  
+
 app.listen(3000);
