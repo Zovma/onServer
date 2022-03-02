afc91c1ef640f908aaee6bd5788527e8794b6e9b
Zovma <dim.borzov@gmail.com>
2022-03-02 11:27:38 +0300

    3 table

--- app.js
+++ app.js
@@ -4,26 +4,63 @@ const app = express();
 // создаем парсер для данных в формате json
 const jsonParser = express.json();
 
-app.post("/result", jsonParser, function (request, response) {
+app.post("/result1", jsonParser, function (request, response) {
     // console.log(request.body);
-    // fs.writeFileSync("result.json", request);
     if (!request.body) return response.sendStatus(400);
     response.json(request.body); // отправляем пришедший ответ обратно
-    // let arr = JSON.parse(request.body);
-    // fs.writeFileSync("result.json", request);
-    // let obj = request.body
-    fs.writeFileSync("result.json", JSON.stringify(request.body));
+
+    fs.writeFileSync("result1.json", JSON.stringify(request.body));
 });
 
-app.get("/", function (request, response) {
+app.post("/result2", jsonParser, function (request, response) {
+    // console.log(request.body);
+    if (!request.body) return response.sendStatus(400);
+    response.json(request.body); // отправляем пришедший ответ обратно
 
-    response.sendFile(__dirname + "/formula.html");
+    fs.writeFileSync("result2.json", JSON.stringify(request.body));
 });
 
-app.get("/json", function (request, response) {
+app.post("/result3", jsonParser, function (request, response) {
+    // console.log(request.body);
+    if (!request.body) return response.sendStatus(400);
+    response.json(request.body); // отправляем пришедший ответ обратно
 
-        response.sendFile(__dirname + "/result.json")
-
+    fs.writeFileSync("result3.json", JSON.stringify(request.body));
 });
+
+app.get("/table1", function (request, response) {
+
+    response.sendFile(__dirname + "/table1.html");
+});
+
+app.get("/table2", function (request, response) {
+
+    response.sendFile(__dirname + "/table2.html");
+});
+
+app.get("/table3", function (request, response) {
+
+    response.sendFile(__dirname + "/table3.html");
+});
+
+app.get("/result1", function (request, response) {
+
+    response.sendFile(__dirname + "/result1.json");
+});
+
+app.get("/result2", function (request, response) {
+
+    response.sendFile(__dirname + "/result2.json");
+});
+
+app.get("/result3", function (request, response) {
+
+    response.sendFile(__dirname + "/result3.json");
+});
+
+
+
+
+
 
 app.listen(3000);
