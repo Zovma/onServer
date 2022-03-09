6446c20e930b21cabc9a16a152e40e111b02db67
Zovma <dim.borzov@gmail.com>
2022-03-09 23:42:16 +0300

    +avgTable

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.post("/result2", jsonParser, function (request, response) {
     response.json(request.body); // отправляем пришедший ответ обратно
 
     fs.writeFileSync("result2.json", JSON.stringify(request.body));
+    fs.writeFileSync("result2.xls", JSON.stringify(request.body));
 });
 
 app.post("/result3", jsonParser, function (request, response) {
@@ -26,6 +27,14 @@ app.post("/result3", jsonParser, function (request, response) {
     response.json(request.body); // отправляем пришедший ответ обратно
 
     fs.writeFileSync("result3.json", JSON.stringify(request.body));
+});
+
+app.post("/avgTable", jsonParser, function (request, response) {
+    // console.log(request.body);
+    if (!request.body) return response.sendStatus(400);
+    response.json(request.body); // отправляем пришедший ответ обратно
+
+    fs.writeFileSync("avgTable.json", JSON.stringify(request.body));
 });
 
 app.get("/table1", function (request, response) {
@@ -42,6 +51,12 @@ app.get("/table3", function (request, response) {
 
     response.sendFile(__dirname + "/table3.html");
 });
+app.get("/avgTable", function (request, response) {
+
+    response.sendFile(__dirname + "/avgTable.html");
+});
+
+
 
 app.get("/result1", function (request, response) {
 
@@ -56,6 +71,11 @@ app.get("/result2", function (request, response) {
 app.get("/result3", function (request, response) {
 
     response.sendFile(__dirname + "/result3.json");
+});
+
+app.get("/avgTableJson", function (request, response) {
+
+    response.sendFile(__dirname + "/avgTable.json");
 });
 
 
