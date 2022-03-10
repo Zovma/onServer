4a7d4aba2d885b5c54b1958eb7cd663db2e91516
Zovma <dim.borzov@gmail.com>
2022-03-10 19:44:45 +0300

    clean server

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.post("/result1", jsonParser, function (request, response) {
     if (!request.body) return response.sendStatus(400);
     response.json(request.body); // отправляем пришедший ответ обратно
 
-    fs.writeFileSync("result1.json", JSON.stringify(request.body));
+    fs.writeFileSync("./indexBuySell/json/result1.json", JSON.stringify(request.body));
 });
 
 app.post("/result2", jsonParser, function (request, response) {
@@ -17,8 +17,7 @@ app.post("/result2", jsonParser, function (request, response) {
     if (!request.body) return response.sendStatus(400);
     response.json(request.body); // отправляем пришедший ответ обратно
 
-    fs.writeFileSync("result2.json", JSON.stringify(request.body));
-    fs.writeFileSync("result2.xls", JSON.stringify(request.body));
+    fs.writeFileSync("./indexBuySell/json/result2.json", JSON.stringify(request.body));
 });
 
 app.post("/result3", jsonParser, function (request, response) {
@@ -26,7 +25,7 @@ app.post("/result3", jsonParser, function (request, response) {
     if (!request.body) return response.sendStatus(400);
     response.json(request.body); // отправляем пришедший ответ обратно
 
-    fs.writeFileSync("result3.json", JSON.stringify(request.body));
+    fs.writeFileSync("./indexBuySell/json/result3.json", JSON.stringify(request.body));
 });
 
 app.post("/avgTable", jsonParser, function (request, response) {
@@ -34,78 +33,154 @@ app.post("/avgTable", jsonParser, function (request, response) {
     if (!request.body) return response.sendStatus(400);
     response.json(request.body); // отправляем пришедший ответ обратно
 
-    fs.writeFileSync("avgTable.json", JSON.stringify(request.body));
+    fs.writeFileSync("./avgTable/avgTableJson/avgTable.json", JSON.stringify(request.body));
 });
 app.post("/avgTable2", jsonParser, function (request, response) {
     // console.log(request.body);
     if (!request.body) return response.sendStatus(400);
     response.json(request.body); // отправляем пришедший ответ обратно
 
-    fs.writeFileSync("avgTable2.json", JSON.stringify(request.body));
+    fs.writeFileSync("./avgTable/avgTableJson/avgTable2.json", JSON.stringify(request.body));
 });
+
 app.post("/avgTable3", jsonParser, function (request, response) {
     // console.log(request.body);
     if (!request.body) return response.sendStatus(400);
     response.json(request.body); // отправляем пришедший ответ обратно
 
-    fs.writeFileSync("avgTable3.json", JSON.stringify(request.body));
+    fs.writeFileSync("./avgTable/avgTableJson/avgTable3.json", JSON.stringify(request.body));
 });
+
+app.post("/avgTable4", jsonParser, function (request, response) {
+    // console.log(request.body);
+    if (!request.body) return response.sendStatus(400);
+    response.json(request.body); // отправляем пришедший ответ обратно
+
+    fs.writeFileSync("./avgTable/avgTableJson/avgTable4.json", JSON.stringify(request.body));
+});
+
+app.post("/avgTable5", jsonParser, function (request, response) {
+    // console.log(request.body);
+    if (!request.body) return response.sendStatus(400);
+    response.json(request.body); // отправляем пришедший ответ обратно
+
+    fs.writeFileSync("./avgTable/avgTableJson/avgTable5.json", JSON.stringify(request.body));
+});
+
+app.post("/avgTable6", jsonParser, function (request, response) {
+    // console.log(request.body);
+    if (!request.body) return response.sendStatus(400);
+    response.json(request.body); // отправляем пришедший ответ обратно
+
+    fs.writeFileSync("./avgTable/avgTableJson/avgTable6.json", JSON.stringify(request.body));
+});
+
+app.post("/avgTable7", jsonParser, function (request, response) {
+    // console.log(request.body);
+    if (!request.body) return response.sendStatus(400);
+    response.json(request.body); // отправляем пришедший ответ обратно
+
+    fs.writeFileSync("./avgTable/avgTableJson/avgTable7.json", JSON.stringify(request.body));
+});
+
+
+
 
 app.get("/table1", function (request, response) {
 
-    response.sendFile(__dirname + "/table1.html");
+    response.sendFile(__dirname + "/indexBuySell/table1.html");
 });
 
 app.get("/table2", function (request, response) {
 
-    response.sendFile(__dirname + "/table2.html");
+    response.sendFile(__dirname + "/indexBuySell/table2.html");
 });
 
 app.get("/table3", function (request, response) {
 
-    response.sendFile(__dirname + "/table3.html");
+    response.sendFile(__dirname + "/indexBuySell/table3.html");
 });
+
+
+
+
 app.get("/avgTable", function (request, response) {
 
-    response.sendFile(__dirname + "/avgTable.html");
+    response.sendFile(__dirname + "/avgTable/avgTable.html");
 });
 app.get("/avgTable2", function (request, response) {
 
-    response.sendFile(__dirname + "/avgTable2.html");
+    response.sendFile(__dirname + "/avgTable/avgTable2.html");
 });
 app.get("/avgTable3", function (request, response) {
 
-    response.sendFile(__dirname + "/avgTable3.html");
+    response.sendFile(__dirname + "/avgTable/avgTable3.html");
+});
+app.get("/avgTable4", function (request, response) {
+
+    response.sendFile(__dirname + "/avgTable/avgTable4.html");
+});
+app.get("/avgTable5", function (request, response) {
+
+    response.sendFile(__dirname + "/avgTable/avgTable5.html");
+});
+app.get("/avgTable6", function (request, response) {
+
+    response.sendFile(__dirname + "/avgTable/avgTable6.html");
+});
+app.get("/avgTable7", function (request, response) {
+
+    response.sendFile(__dirname + "/avgTable/avgTable7.html");
 });
 
 
 
 app.get("/result1", function (request, response) {
 
-    response.sendFile(__dirname + "/result1.json");
+    response.sendFile(__dirname + "/indexBuySell/json/result1.json");
 });
 
 app.get("/result2", function (request, response) {
 
-    response.sendFile(__dirname + "/result2.json");
+    response.sendFile(__dirname + "/indexBuySell/json/result2.json");
 });
 
 app.get("/result3", function (request, response) {
 
-    response.sendFile(__dirname + "/result3.json");
+    response.sendFile(__dirname + "/indexBuySell/json/result3.json");
 });
+
+
+
+
 
 app.get("/avgTableJson", function (request, response) {
 
-    response.sendFile(__dirname + "/avgTable.json");
+    response.sendFile(__dirname + "/avgTable/avgTableJson/avgTable.json");
 });
 app.get("/avgTableJson2", function (request, response) {
 
-    response.sendFile(__dirname + "/avgTable2.json");
+    response.sendFile(__dirname + "/avgTable/avgTableJson/avgTable2.json");
 });
 app.get("/avgTableJson3", function (request, response) {
 
-    response.sendFile(__dirname + "/avgTable3.json");
+    response.sendFile(__dirname + "/avgTable/avgTableJson/avgTable3.json");
+});
+app.get("/avgTableJson4", function (request, response) {
+
+    response.sendFile(__dirname + "/avgTable/avgTableJson/avgTable4.json");
+});
+app.get("/avgTableJson5", function (request, response) {
+
+    response.sendFile(__dirname + "/avgTable/avgTableJson/avgTable5.json");
+});
+app.get("/avgTableJson6", function (request, response) {
+
+    response.sendFile(__dirname + "/avgTable/avgTableJson/avgTable6.json");
+});
+app.get("/avgTableJson7", function (request, response) {
+
+    response.sendFile(__dirname + "/avgTable/avgTableJson/avgTable7.json");
 });
 
 
