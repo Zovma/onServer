2c16f9814ad3ac92c4e53651e00c3ecbb13199cc
Zovma <dim.borzov@gmail.com>
2022-03-11 19:23:06 +0300

    +script

--- app.js
+++ app.js
@@ -84,6 +84,11 @@ app.post("/avgTable7", jsonParser, function (request, response) {
 });
 
 
+app.get("/script", function (request, response) {
+
+    response.sendFile(__dirname + "/script.html");
+});
+
 
 
 app.get("/table1", function (request, response) {
